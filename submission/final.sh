169259ea23bb7cfa2f7a665db3b6f6011a5abc9d
MAC <mac@MACs-MacBook-Pro.local>
2026-03-19 18:06:34 +0100

    week1-exercise

--- submission/final.sh
+++ submission/final.sh
@@ -22,12 +22,13 @@ echo "----------------------------------------"
 echo "Create a wallet named 'btrustwallet' to track your Bitcoin exploration"
 # STUDENT TASK: Use bitcoin-cli to create a wallet named "btrustwallet"
 # WRITE YOUR SOLUTION BELOW:
-
+bitcoin-cli createwallet "btrustwallet"
 
 # Create a second wallet that will hold the treasure
 echo "Now, create another wallet called 'treasurewallet' to fund your adventure"
 # STUDENT TASK: Create another wallet called "treasurewallet"
 # WRITE YOUR SOLUTION BELOW:
+bitcoin-cli createwallet "treasurewallet"
 
 
 # Generate an address for mining in the treasure wallet
